01126f1acd7092230330f54c23fc787b6ff14c09
brendp252 <brendp@umich.edu>
2026-07-05 14:23:31 -0400

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 3306,
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
