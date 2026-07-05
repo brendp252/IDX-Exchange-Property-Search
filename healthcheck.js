2273f1e02b5ab07fb32172a9ef6bdfb03fb19cad
brendp252 <brendp@umich.edu>
2026-07-05 14:25:23 -0400

    Update healthcheck.js

--- healthcheck.js
+++ healthcheck.js
@@ -1,14 +1,21 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const pool = require("./db");
 
 app.get("/api/health", async(req, res) => {
     try {
-        await pool.getConnection();
         await pool.query("SELECT 1");
         res.status(200).json({ status: "ok", database: "connected" });
     }
-    catch (err) {
-        res.status(500).json({ status: "error" });
+    catch (error) {
+        console.error("Failed to connect to database");
+        res.status(500).json({ status: "error", database: "not connected" });
     }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
