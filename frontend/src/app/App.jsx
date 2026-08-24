bf50cbc53a3f772ab5ac90750b74fe5fa4b16b30
brendp252 <brendp@umich.edu>
2026-08-24 15:52:07 -0400

    Add files via upload

---
+++ frontend/src/app/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router';
+import ListingsPage from '../pages/ListingsPage';
+import PropertyDetailPage from '../pages/PropertyDetailPage';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ListingsPage />} />
+        <Route path="/property/:id" element={<PropertyDetailPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
