5d60ae2bf4a5be178dde3ff9040587d277efcd9f
brendp252 <brendp@umich.edu>
2026-08-24 15:52:52 -0400

    Update client.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -12,6 +12,20 @@ export async function fetchPropertyDetail(id) {
     }
 }
 
+export async function fetchPropertyOpenhouses(id) {
+    try {
+        const url = `http://localhost:5000/api/properties/${id}/openhouses`;
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch ${url}`);
+        }
+        return response.json();
+    }
+    catch (error) {
+        throw new Error("Unable to reach server");
+    }
+}
+
 export async function fetchProperties(params = {}) {
     try {
         const query = new URLSearchParams(params).toString();
