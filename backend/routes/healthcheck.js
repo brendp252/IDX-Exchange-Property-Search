14d3cb40df159eadca694ba764c9e76377f27825
brendp252 <brendp@umich.edu>
2026-09-07 15:39:13 -0400

    Update healthcheck.js

--- backend/routes/healthcheck.js
+++ backend/routes/healthcheck.js
@@ -6,11 +6,11 @@ router.get("/", async(req, res) => {
     try {
         await pool.query("SELECT 1");
         console.log("Connected to database");
-        res.status(200).json({ status: "ok", database: "connected" });
+        return res.status(200).json({ status: "ok", database: "connected" });
     }
     catch (error) {
         console.error("Failed to connect to database");
-        res.status(500).json({ status: "error", database: "not connected" });
+        return res.status(500).json({ status: "error", database: "not connected" });
     }
 });
 
